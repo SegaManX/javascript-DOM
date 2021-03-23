c14cbb3fd8709423720287f8791954e49ecb5f7b
SegaManX <karlis.egle@gmail.com>
2021-03-23 15:49:21 +0200

    5. uzdevums

--- 05-toggle-elements-of-dom-path/global.js
+++ 05-toggle-elements-of-dom-path/global.js
@@ -1,1 +1,15 @@
-// Your JavaScript goes here.
+window.addEventListener("load", function(){
+    
+    document.getElementById("toggle_button").addEventListener("click", function(){
+        var x = document.querySelectorAll(".second_five li");
+        
+        for(i = 0; i < x.length; i++)
+        {
+        if (x[i].style.display === "none") {
+            x[i].style.display = "block";
+        } else {
+            x[i].style.display = "none";
+        }
+    }
+    });
+});
