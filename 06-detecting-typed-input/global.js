c5751f56db5d2c0332a39ec34bfddf75c5c66a12
SegaManX <karlis.egle@gmail.com>
2021-03-23 15:52:29 +0200

    6. uzdevums un 5. jaut

--- 06-detecting-typed-input/global.js
+++ 06-detecting-typed-input/global.js
@@ -1,1 +1,13 @@
-// Your JavaScript goes here.
+window.addEventListener("load", function(){
+
+    var x = document.getElementById("full_name");
+  
+    x.addEventListener("keyup", function() {
+  
+      var y = document.getElementById("greeting");
+  
+      y.innerHTML = ("Hello there, " + name_field.value + "!");
+  
+    });
+  
+  });
