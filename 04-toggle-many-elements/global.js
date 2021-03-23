b738822d3d4bf48da57fc7610fd183babe369903
SegaManX <karlis.egle@gmail.com>
2021-03-23 15:01:48 +0200

    4. uzdevums

--- 04-toggle-many-elements/global.js
+++ 04-toggle-many-elements/global.js
@@ -1,1 +1,14 @@
-// Your JavaScript goes here.
+window.addEventListener("load", function(){
+    
+    document.getElementById("button").addEventListener("click", function(){
+        var x = document.getElementsByClassName("toggle_me");
+        for(var i=0; i < x.length; i++)
+        {
+            if (x[i].style.display == "none") {
+                x[i].style.display = "block";
+            } else {
+                x[i].style.display = "none";
+            }
+        }
+    });
+});
